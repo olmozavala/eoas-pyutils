4112b69b73de676239bdb2ed5d4e84f32f8f2194
Olmo Zavala <olmozavala@gmail.com>
2022-05-13 11:45:04 -0400

    Added video generation from folder images and normalizatoin opetion (log,log plot)

--- viz_utils/eoa_viz.py
+++ viz_utils/eoa_viz.py
@@ -1,9 +1,11 @@
+import os
+from PIL import Image
+import cv2
 from os import listdir
 from os.path import join
 
 import matplotlib.pyplot as plt
 import matplotlib
-from matplotlib.colors import LogNorm
 from proj_utils.common import create_folder
 from viz_utils.constants import PlotMode, BackgroundType
 import pylab
@@ -55,7 +57,7 @@ class EOAImageVisualizer:
     _auto_colormap = True  # Selects the colormap based on the name of the field
     _show_var_names = False  # Includes the name of the field name in the titles
     _additional_polygons = []  # MUST BE SHAPELY GEOMETRIES In case we want to include additional polygons in the plots (all of them)
-
+    _norm = None  # Use to normalize the colormap. For example with LogNorm
     def __init__(self, disp_images=True, output_folder='output',
                  lats=[-90,90], lons =[-180,180],
                  projection=ccrs.PlateCarree(), **kwargs):
@@ -122,9 +124,9 @@ class EOAImageVisualizer:
                 im = c_ax.contourf(self._lons, self._lats, c_img, num_colors=255, cmap='inferno', extent=self._extent)
             else:
                 if np.isnan(mincbar):
-                    im = c_ax.imshow(c_img, extent=self._extent, origin=origin, cmap=cmap, transform=self._projection)
+                    im = c_ax.imshow(c_img, extent=self._extent, origin=origin, cmap=cmap, transform=self._projection, norm=self._norm)
                 else:
-                    im = c_ax.imshow(c_img, extent=self._extent, origin=origin, cmap=cmap, vmin=mincbar, vmax=maxcbar, transform=self._projection)
+                    im = c_ax.imshow(c_img, extent=self._extent, origin=origin, cmap=cmap, vmin=mincbar, vmax=maxcbar, transform=self._projection, norm=self._norm)
 
         if mode == PlotMode.CONTOUR or mode == PlotMode.MERGED:
             c_ax.set_extent(self.getExtent(list(self._lats), list(self._lons)))
@@ -258,7 +260,7 @@ class EOAImageVisualizer:
         return ax
 
     def plot_3d_data_npdict(self, np_variables:list, var_names:list, z_levels= [], title='',
-                          file_name_prefix='', cmap='viridis', z_names = [],
+                          file_name_prefix='', cmap=None, z_names = [],
                           show_color_bar=True, plot_mode=PlotMode.RASTER, mincbar=np.nan, maxcbar=np.nan):
         """
         Plots multiple z_levels for multiple fields.
@@ -301,7 +303,7 @@ class EOAImageVisualizer:
                 if not(np.all(np.isnan(maxcbar))):
                     c_maxcbar = maxcbar[idx_var]
 
-                if self._auto_colormap:
+                if self._auto_colormap and cmap is None:
                     cmap = select_colormap(c_var)
 
                 im = self.plot_slice_eoa(np_variables[c_var][c_slice,:,:], ax, cmap=cmap, mode=plot_mode,
@@ -383,3 +385,24 @@ class EOAImageVisualizer:
                         file_name_prefix=file_name_prefix, cmap=cmap, z_names = [],
                         show_color_bar=show_color_bar, plot_mode=plot_mode, mincbar=mincbar, maxcbar=maxcbar)
 
+
+    def make_video_from_images(self, input_folder, output_file, fps=24):
+        files = listdir(input_folder)
+        files.sort()
+
+        print(F"Generating video file: {output_file}")
+        out_video = -1
+        for i, file_name in enumerate(files[0:36]):
+            if i % 10 == 0:
+                print(F"Adding file # {i}: {file_name}")
+            c_file = join(input_folder, file_name)
+            im = Image.open(c_file)
+            np_im = np.asarray(im)[:, :, :3]
+            if i == 0:
+                video_size = (np_im.shape[1], np_im.shape[0])
+                out_video = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, video_size, True)
+            out_video.write(np_im[:, :, ::-1])
+
+        out_video.release()
+        cv2.destroyAllWindows()
+        print("Done! yeah babe!")
