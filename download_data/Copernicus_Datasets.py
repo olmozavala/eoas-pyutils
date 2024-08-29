495ddb881f10eb1769f08b64b96d68dc569d4311
Olmo Zavala <olmozavala@gmail.com>
2024-08-29 15:43:33 -0400

    Tested in local machine, it works

--- download_data/Copernicus_Datasets.py
+++ download_data/Copernicus_Datasets.py
@@ -5,7 +5,8 @@ from enum import Enum
 
 class Copernicus_Enum(Enum):
     # Ch
-    CHLORA_L3_D = 1 # https://data.marine.copernicus.eu/product/OCEANCOLOUR_GLO_BGC_L3_MY_009_103/description
+    CHLORA_L3_OLCI_300M_2016 = 0 # https://data.marine.copernicus.eu/product/OCEANCOLOUR_GLO_BGC_L3_MY_009_103/description
+    CHLORA_L3_OLCI_4KM_2016: 1 # https://data.marine.copernicus.eu/product/OCEANCOLOUR_GLO_BGC_L3_MY_009_103/description
     CHLORA_L4_D_1997 = 2 # https://data.marine.copernicus.eu/product/OCEANCOLOUR_GLO_BGC_L4_MY_009_104/description
 
     SST_ODYSSEA_L3_D = 20 # https://data.marine.copernicus.eu/product/SST_GLO_SST_L3S_NRT_OBSERVATIONS_010_010/description
@@ -35,15 +36,25 @@ Copernicus_Datasets = {
         'resolution':'0.25x0.25',
         'variables': ["adt", "err_sla", "err_ugosa", "err_vgosa", "flag_ice", "sla", "tpa_correction", "ugos", "ugosa", "vgos", "vgosa"],
     },
-    Copernicus_Enum.CHLORA_L3_D: {
+    Copernicus_Enum.CHLORA_L3_OLCI_300M_2016: {
         'name': 'Global Ocean Colour, Bio-Geo-Chemical, L3 (daily) from Satellite Observations (1997-ongoing)',
-        'short_name': 'Ocean_Color_L3',
+        'short_name': 'Ocean_Color_L3_OLCI_300M',
         'id': 'cmems_obs-oc_glo_bgc-plankton_my_l3-olci-300m_P1D',
         'version': '202211',
-        'start_date': '1997',
-        'resolution': '4km 0.036',
+        'start_date': '2016-04-01',
+        'resolution': '300m 0.0027',
         'variables': ["CHL", "CHL_uncertainty", "flags"],
     },
+    Copernicus_Enum.CHLORA_L3_OLCI_4KM_2016: {
+        'name': 'Global Ocean Colour, Bio-Geo-Chemical, L3 (daily) from Satellite Observations (1997-ongoing)',
+        'short_name': 'Ocean_Color_L3_OLCI_4km',
+        'id': 'cmems_obs-oc_glo_bgc-plankton_my_l3-olci-4km_P1D',
+        'version': '202207',
+        'start_date': '2016-04-01',
+        'resolution': '300m 0.0027',
+        'variables': ["CHL", "CHL_uncertainty", "flags"],
+    },
+
     Copernicus_Enum.CHLORA_L4_D_1997: {
         'name': 'Global Ocean Colour (Copernicus-GlobColour), Bio-Geo-Chemical, L4 (monthly and interpolated) from Satellite Observations (1997-ongoing)',
         'short_name': 'Ocean_Color',
--- download_data/Download_COPERNICUS.py
+++ download_data/Download_COPERNICUS.py
@@ -30,52 +30,22 @@ def download_by_year(year, cop_ds, bbox, output_folder):
     else:
         output_filename = f"{start_date.year}.nc"
 
-    # cm.subset(
-    #     dataset_id=cop_ds['id'],
-    #     dataset_version=cop_ds['version'],
-    #     variables= cop_ds['variables'],
-    #     minimum_longitude=bbox[0],
-    #     maximum_longitude=bbox[2],
-    #     minimum_latitude=bbox[1],
-    #     maximum_latitude=bbox[3],
-    #     start_datetime=start_date.strftime("%Y-%m-%dT00:00:00"),
-    #     end_datetime=end_date.strftime("%Y-%m-%dT00:00:00"),
-    #     output_filename=output_filename,
-    #     output_directory=output_folder,
-    #     username=username,
-    #     password=password,
-    #     force_download=True
-    # )
-
-    # cm.subset(
-    # dataset_id="cmems_obs-oc_glo_bgc-plankton_my_l3-olci-300m_P1D",
-    # dataset_version="202211",
-    # variables=["CHL", "CHL_uncertainty", "flags"],
-    # minimum_longitude=bbox[0],
-    # maximum_longitude=bbox[2],
-    # minimum_latitude=bbox[1],
-    # maximum_latitude=bbox[3],
-    # start_datetime=start_date.strftime("%Y-%m-%dT00:00:00"),
-    # end_datetime=end_date.strftime("%Y-%m-%dT00:00:00"),
-    # force_download=True,
-    # subset_method="strict",
-    # disable_progress_bar=True,
-    # )
-
     cm.subset(
-  dataset_id="cmems_obs-oc_glo_bgc-plankton_my_l3-olci-300m_P1D",
-  dataset_version="202211",
-  variables=["CHL", "CHL_uncertainty", "flags"],
-  minimum_longitude=-179.99722290039062,
-  maximum_longitude=179.9972381591797,
-  minimum_latitude=-89.99722290039062,
-  maximum_latitude=89.99722290039062,
-  start_datetime="2024-08-20T00:00:00",
-  end_datetime="2024-08-20T00:00:00",
-  force_download=True,
-  subset_method="strict",
-  disable_progress_bar=True,
-)
+        dataset_id=cop_ds['id'],
+        dataset_version=cop_ds['version'],
+        variables= cop_ds['variables'],
+        minimum_longitude=bbox[0],
+        maximum_longitude=bbox[2],
+        minimum_latitude=bbox[1],
+        maximum_latitude=bbox[3],
+        start_datetime=start_date.strftime("%Y-%m-%dT00:00:00"),
+        end_datetime=end_date.strftime("%Y-%m-%dT00:00:00"),
+        output_filename=output_filename,
+        output_directory=output_folder,
+        username=username,
+        password=password,
+        force_download=True
+    )
 
 # %% -------- Download data by month ----------
 def download_by_month(year, cop_ds, bbox, output_folder):
@@ -109,18 +79,16 @@ def download_by_month(year, cop_ds, bbox, output_folder):
 bbox_gom = (-98.25, 7.25, -55.0, 50.0) # DO NOT DELETE THIS LINE, THIS BBOX ARE IMPORTANT!
 bbox_global = (-180, -90, 180, 90) # DO NOT DELETE THIS LINE, THIS BBOX ARE IMPORTANT!
 
-bbox = bbox_global
+bbox = bbox_gom
 
-# cop_ds = Copernicus_Datasets[Copernicus_Enum.CHLORA_L3_D]
-cop_ds = Copernicus_Datasets[Copernicus_Enum.SSH_DUACS_L4_D_1993]
+cop_ds = Copernicus_Datasets[Copernicus_Enum.CHLORA_L3_OLCI_300M_2016]
 
 # output_folder = "/unity/f1/ozavala/DATA/GOFFISH/CHLORA/COPERNICUS"
 # output_folder = "/tmp/OZ/"
-output_folder = "/unity/f1/ozavala/DATA/GOFFISH/CHLORA/COPERNICUS_L3"
+output_folder = "/home/olmozavala/Downloads/Chlora"
 
-for c_year in range(1997, 2024):
-    download_by_year(c_year, cop_ds, bbox_global, output_folder)
-    break
+for c_year in range(2016, 2025):
+    download_by_year(c_year, cop_ds, bbox, output_folder)
     # download_by_month(c_year, cop_ds, bbox, output_folder)
 
 # %% TODO Understand: 
